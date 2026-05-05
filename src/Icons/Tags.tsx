c08feb6960bd7c6746209ca79864642c1fec2031
Sawal Timsina <t.sawal231@gmail.com>
2026-05-05 11:43:17 +0545

    Migrate Tag icon to TypeScript and update jsconfig for React JSX support

---
+++ src/Icons/Tags.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import { IconType } from '../types';
+import createIcon from '../helpers/createIcon';
+
+const Tags: React.FC<IconType> = ({ color, strokeWidth, set }) => {
+  const Broken = () => (
+    <g>
+      <path d="M5 15L2 12L8 6H13" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M8 18H21V6H18" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+      <path
+        d="M12 12C12 13.1046 11.1046 14 10 14C8.89543 14 8 13.1046 8 12C8 10.8954 8.89543 10 10 10C11.1046 10 12 10.8954 12 12Z"
+        stroke={color}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+  );
+  const Curved = () => (
+    <g>
+      <path
+        d="M11 12C11 12.8284 10.3284 13.5 9.5 13.5C8.67157 13.5 8 12.8284 8 12C8 11.1716 8.67157 10.5 9.5 10.5C10.3284 10.5 11 11.1716 11 12Z"
+        stroke={color}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21 16V8C21 6.89543 20.1046 6 19 6H8.82843C8.298 6 7.78929 6.21071 7.41422 6.58579L3.41422 10.5858C2.63317 11.3668 2.63317 12.6332 3.41422 13.4142L7.41422 17.4142C7.78929 17.7893 8.298 18 8.82843 18H19C20.1046 18 21 17.1046 21 16Z"
+        stroke={color}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+  );
+  const Duotone = () => (
+    <g>
+      <path
+        opacity="0.15"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M22 6H9L3 12L9 18H22V6ZM11 13.5C11.8284 13.5 12.5 12.8284 12.5 12C12.5 11.1716 11.8284 10.5 11 10.5C10.1716 10.5 9.5 11.1716 9.5 12C9.5 12.8284 10.1716 13.5 11 13.5Z"
+        fill={color}
+      />
+      <path d="M21 18V6H8L2 12L8 18H21Z" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+      <path
+        d="M12 12C12 13.1046 11.1046 14 10 14C8.89543 14 8 13.1046 8 12C8 10.8954 8.89543 10 10 10C11.1046 10 12 10.8954 12 12Z"
+        stroke={color}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+  );
+  const Outline = () => (
+    <g>
+      <path d="M21 18V6H8L2 12L8 18H21Z" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+      <path
+        d="M12 12C12 13.1046 11.1046 14 10 14C8.89543 14 8 13.1046 8 12C8 10.8954 8.89543 10 10 10C11.1046 10 12 10.8954 12 12Z"
+        stroke={color}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+  );
+
+  switch (set) {
+    case 'broken':
+      return <Broken />;
+    case 'curved':
+      return <Curved />;
+    case 'duotone':
+      return <Duotone />;
+    case 'outline':
+      return <Outline />;
+    default:
+      return <Outline />;
+  }
+};
+
+Tags.displayName = 'Tags';
+export default createIcon(Tags);
